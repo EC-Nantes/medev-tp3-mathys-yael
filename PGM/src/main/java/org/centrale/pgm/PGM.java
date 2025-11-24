3f8087ae95db75ba652462b76fb50b017868a5c5
Yaël <yael.vincent@orange.fr>
2025-11-24 17:40:16 +0100

    Création de la classe PGMImage

---
+++ PGM/src/main/java/org/centrale/pgm/PGM.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.pgm;
+
+/**
+ *
+ * @author yaelv
+ */
+public class PGM {
+
+    
+}
