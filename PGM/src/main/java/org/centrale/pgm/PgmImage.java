e01d5528d3ee648684592325190599044d26f422
Yaël <yael.vincent@orange.fr>
2025-12-08 15:00:14 +0100

    suite code méthode save

--- PGM/src/main/java/org/centrale/pgm/PgmImage.java
+++ PGM/src/main/java/org/centrale/pgm/PgmImage.java
@@ -6,6 +6,7 @@ package org.centrale.pgm;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  *
@@ -28,16 +29,29 @@ public class PgmImage {
          try (BufferedWriter writer = new BufferedWriter(new FileWriter(chemin))) {
             writer.write("P2");
             writer.newLine();
-            writer.write("#");
+            writer.write(comment);
             writer.newLine();
             writer.write(l + " " + h);
             writer.newLine();
+            writer.write(255);
+            writer.newLine();
+            int nb_carac = 0;
+            for(int i=0;i<h;i++){
+                for(int j=0;j<l;j++){
+                    writer.write(pixels[i][j] + " ");
+                    nb_carac += 4;
+                    if(nb_carac > 66){
+                        writer.newLine();
+                        nb_carac = 0;
+                    }
+                }
+            }
             
             System.out.println("Fichier créé avec succès !");
         } catch (IOException e) {
             System.err.println("Erreur lors de la création du fichier : " + e.getMessage());
         }
     }
-    }
-    
 }
+    
+
